1128f97eae9a64522cffeb4901d5024ac9daa8dd
Aiman Baharna <ab@clearmatics.com>
2025-08-11 18:31:31 +0300

    Fix Trezor device support in devenv
    After upgrading Hatch in a recent devenv upgrade, it does not seem
    possible to load `libusb1` in Hatch environments.
    
    This was likely due to Hatch running a Python version that no longer
    matched the devenv Python.

--- devenv.nix
+++ devenv.nix
@@ -1,17 +1,25 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
   packages = [
     pkgs.hatch
     pkgs.taplo
+    pkgs.libusb1
   ];
 
   env.HATCH_PYTHON = "${config.env.DEVENV_PROFILE}/bin/python";
+  env.LD_LIBRARY_PATH = lib.makeLibraryPath [
+    pkgs.libusb1
+  ];
 
   languages.python = {
     enable = true;
-    version = "3.12";
+    version = "3.13"; # must match `pkgs.hatch` python version
     venv.enable = false;
-    libraries = [ pkgs.libusb1 ];
   };
 }
